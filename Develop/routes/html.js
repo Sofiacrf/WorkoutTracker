8510494eda71522fa9e005a7ac848063a0ba1821
Sofia Carolina Rojas Flores <sofia.rojasflo@gmail.com>
2021-07-23 19:28:49 -0500

    updted route

--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { Router } = require("express");
 const path = require("path");
 
-router.get("/exercise", (req,res) => {
+router.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"))
 })
 
